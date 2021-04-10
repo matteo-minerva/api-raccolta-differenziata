30c012d3c4e13d5e0eb349753d3bc59fd10af792
Matteo Minerva <m.minerva@outlook.it>
2021-04-10 19:26:35 +0200

    Removed user table

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ApiRaccoltaController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
  */
-
-Route::middleware( 'auth:api' )->get( '/user', function ( Request $request ) {
-    return $request->user();
-} );
 
 Route::get( "/all", [ApiRaccoltaController::class, "index"] );
 Route::post( "/add", [ApiRaccoltaController::class, "add"] );
